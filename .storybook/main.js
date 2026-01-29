3a090c511e42664cbc95f250be12594995f12e6d
끄댕이 <jgpark812@gmail.com>
2026-01-29 15:20:35 +0900

    초기셋팅완료

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+
+export default config;
